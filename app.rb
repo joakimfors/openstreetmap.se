f64a02fde94ee6f92dd3c93e37d5eb0de5a58524
Joakim Fors <joakim@fo.rs>
2013-11-09 17:44:12 +0100

    Add stub app.rb

---
+++ app.rb
@@ -0,0 +1,12 @@
+# Use the app.rb file to load Ruby code, modify or extend the models, or
+# do whatever else you fancy when the theme is loaded.
+
+module Nesta
+  class App
+    helpers do
+      # Add new helpers here.
+    end
+
+    # Add new routes here.
+  end
+end
